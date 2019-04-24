adec869c9e38f0dadc5b4dbf11a5d6b67e0c4c52
Tony <schmittaj@gmail.com>
2019-04-24 14:27:40 -0500

    Improvement, but still needs threadpool for server

--- Client.py
+++ Client.py
@@ -1,91 +1,172 @@
-#********************************************
+#####################################
 # Client.py
-#*********************************************
+#####################################
+import threading
 from socket import *
 
+global loggedIn
 loggedIn = False
-global clientToServer
+global userID
 
 
-#************************************
+#####################################
 # Function: logout
-#************************************
-def logout(clientSocket):
-    print("Quitting...Bye Bye!")
-    clientSocket.send("QUIT".encode())
-    clientSocket.shutdown(1)
-    clientSocket.close()
-    exit(0)
-# End of function logout
-
-#************************************
-# Function: message
-#************************************
-def message(clientSocket):
-    msg = input("Type you message:\t")
-    msg = "MESSAGE\t"+msg
+#####################################
+def logout():
+    clientSocket = server_connect()
+    print("Logging out.")
+    msg = "LOGOUT\t" + userID
     msg = msg.encode()
     clientSocket.send(msg)
-    print("Message sent")
     response = clientSocket.recv(4096)
-    response.decode()
-    print(str(response))
+    response = response.decode()
+    if response.split("\t")[1].upper() == "FALSE":
+        print("Failed to Logout")
+    else:
+        print("Logged out")
+        global loggedIn
+        loggedIn = False
+    clientSocket.close()
+# End of function logout
+
+
+#####################################
+# Function: message
+#####################################
+def message():
+    clientSocket = server_connect()
+    toUser = input("Who are you sending a message to:\t")
+    msg = input("Type you message:\t")
+    msg = "MESSAGE\t"+userID+"\t"+toUser+"\t"+msg
+    msg = msg.encode()
+    clientSocket.send(msg)
+    response = clientSocket.recv(4096)
+    response = response.decode()
+    if response.split("\t")[3].upper() == "FALSE":
+        print("Message failed to send to " + toUser + ".")
+    clientSocket.close()
 # End of function message
 
-#************************************
+#####################################
 # Function: login
-#************************************
+#####################################
 def login():
-    global clientToServer
     clientToServer = server_connect()
-    userid = input("Please enter your userid:\t")
-    msg = "LOGIN"+"\t"+userid+"\t"
+    global userID
+    userID = input("Please enter your userid:\t")
+    msg = "LOGIN"+"\t"+userID
     msg = msg.encode()
     clientToServer.send(msg)
     response = clientToServer.recv(4096)
     response = response.decode()
-    if("BAD" in response.upper()):
+    clientToServer.close()
+    if("FALSE" in response.upper()):
         print("Failed to login", response)
     else:
-        nice = response.split("\t")[0] + "\t" +response.split("\t")[0]
-        print(nice)
+        nice = response.split("\t")[0] + "\t" +response.split("\t")[1]
+        print("Logged In")
         global loggedIn
         loggedIn = True
 # End of function login
 
 
-#************************************
+#####################################
+# Function: userList
+#####################################
+def userList():
+    clientSocket = server_connect()
+    msg = "USERLIST"
+    msg = msg.encode()
+    clientSocket.send(msg)
+    response = clientSocket.recv(4096)
+    response = response.decode()
+    for a in response.split("\t"):
+        print(a)
+    clientSocket.close()
+# End of function userList
+
+
+#####################################
 # Function: server_connect
-#************************************
+#####################################
 def server_connect():
     serverName = "127.0.0.1"
     serverPort = 12012
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName,serverPort))
-    print("Connection successful, ready to communicate")
     return clientSocket
-#End of function server_connect
+# End of function server_connect
 
-#Main Program Loop
-while(True):
-    if(loggedIn):
-        print("1 - message")
-        print("0 - for log-out")
-        print(" ")
-        choice = int(input("Your choice is: "))
-        print(type(choice))
-        if choice == 1:
-            message(clientToServer)
-        elif choice == 0:
-            logout(clientToServer)
-            break
+
+#####################################
+# Function: handler
+#####################################
+def handler(connectionSocket):
+    message = connectionSocket.recv(4096)
+    message = message.decode()
+    wholeMsg = message.split("\t")
+    if wholeMsg[0] == "MESSAGE":
+        if wholeMsg[2] == userID:
+            print("Message from user " + wholeMsg[1])
+            print(wholeMsg[3])
+            response = "MESSAGE\t" + wholeMsg[1] + "\t" + userID + "\tTrue"
+            response = response.encode()
+            connectionSocket.send(response)
+        else:
+            print("This message was not meant for you")
     else:
-        print("1 - for log-in")
-        print("0 - for log-out")
-        print(" ")
-        choice = int(input("Your choice is: "))
-        if choice == 1:
-            login()
-        elif choice == 0:
-            logout()
-            break
+        print("Error")
+    connectionSocket.close()
+# End of function handler
+
+
+#####################################
+# Function: inputListen
+#####################################
+def inputListen():
+    listeningPort = 12013
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.bind(('', listeningPort))
+    clientSocket.listen(10)
+    while True:
+        connectionSocket, address = clientSocket.accept()
+        handler(connectionSocket)
+# End of function inputListen
+
+
+#####################################
+# Function: userLoop
+#####################################
+def userLoop():
+    while(True):
+        if(loggedIn):
+            print("1 - message")
+            print("2 - get list of online users")
+            print("0 - for log-out")
+            print(" ")
+            choice = int(input("Your choice is: "))
+            if choice == 1:
+                message()
+            elif choice ==2:
+                userList()
+            elif choice == 0:
+                logout()
+        else:
+            print("1 - for log-in")
+            print("0 - for log-out")
+            print(" ")
+            choice = int(input("Your choice is: "))
+            if choice == 1:
+                login()
+            elif choice == 0:
+                exit(0)
+# End of function userLoop
+
+
+# Main Program
+global listeningThread
+listeningThread = threading.Thread(target=inputListen)
+listeningThread.daemon = True
+userThread = threading.Thread(target=userLoop)
+listeningThread.start()
+userThread.start()
--- Server.py
+++ Server.py
@@ -1,68 +1,163 @@
-#********************************************
+#####################################
 # Server.py
-#*********************************************
+#####################################
 from socket import *
 from datetime import datetime
-import _thread
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+global userListLock
+global userInfo
 
 
-#************************************
+#####################################
 # Function: loginProcessing
-#************************************
-def loginProcessing(request, connectionSocket):
-    loginStatus = "Failure" # A client was not logged in when just connected
-    #Strip information and print in line for debugging
+#####################################
+def loginProcessing(request, connectionSocket, address):
     userid = request.split('\t')[1].strip()
-
-    print('userID:  '+userid)
-    USERIDSTATUS = "GOOD"
-    response = "USERIDSTATUS: " + USERIDSTATUS
+    input = (userid, address)
+    good = add_user_to_list(input)
+    response = "LOGIN\t" + str(good)
     response = response.encode()
     connectionSocket.send(response)
-    output_file = open("Log.txt", "a")
-    output_file.write(datetime.now().strftime("%H:%M:%S %B %d, %Y"))
-    output_file.write("\t" + "Login Success" + "\t" + userid + "\n")
-    output_file.close()
-    return(True)
-
+    connectionSocket.close()
+    return good
 # End of function loginProcessing
 
 
-#************************************
+#####################################
 # Function: messageProcessing
-#************************************
+#####################################
 def messageProcessing(request,connectionSocket):
-    print("processing Message")
-    msg = request.split("\t")[1]
-    msg = msg.encode()
-    connectionSocket.send(msg)
+    sender = request.split("\t")[1]
+    receiver = request.split("\t")[2]
+    msg = request.split("\t")[3]
+    outgoing = "MESSAGE\t" + sender + "\t" + receiver + "\t" + msg
+    outgoing = outgoing.encode()
+    addr = get_user_socket(receiver)
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((addr,12013))
+    clientSocket.send(outgoing)
+    ack = clientSocket.recv(4096)
+    ack = ack.decode()
+    ackMsg = ack.split("\t")[3]
+    clientSocket.close()
+    resp = "MESSAGE\t" + sender + "\t" + receiver + "\t" + ackMsg
+    resp = resp.encode()
+    connectionSocket.send(resp)
+    connectionSocket.close()
 # End of function messageProcessing
 
 
-#************************************
-# Function: handler
-#************************************
-def handler(connectionSocket):
-    close = False
-    while not close:
-        print("waiting on client")
-        message = connectionSocket.recv(4096)
-        message = message.decode()
-        request = message.split("\t")[0]
-        print("Request message:", request)
-        if(request.upper() == "QUIT"):
-            close = True
-        else:
-            #Parse the method name from the request message
-            methodName = request.split('\t')[0].strip()
-            print("From", addr,methodName)
+#####################################
+# Function: getListProcessing
+#####################################
+def getListProcessing(connectionSocket):
+    lst = get_user_list()
+    message = "USERLIST\t"
+    for a in lst:
+        message += str(a) + "\t"
+    message = message.encode()
+    connectionSocket.send(message)
+    connectionSocket.close()
+# End of function getListProcessing
 
-            if(methodName.upper() == "LOGIN"):
-                loginProcessing(message, connectionSocket)
-            elif(methodName.upper() == "MESSAGE"):
-                messageProcessing(message,connectionSocket)
-            else:
-                print("Something went wrong")
+
+#####################################
+# Function: logoutProcessing
+#####################################
+def logoutProcessing(message,connectionSocket):
+    userid = message.split('\t')[1].strip()
+    success = remove_user_from_list(userid)
+    response = "LOGOUT\t" + str(success)
+    response = response.encode()
+    connectionSocket.send(response)
+    connectionSocket.close()
+# End of function logoutProcessing
+
+
+#####################################
+# Function: add_user_to_list
+#####################################
+def add_user_to_list(userName):
+    userListLock.acquire()
+    alreadyExists = False
+    for a in range(0,len(userInfo)):
+        if userInfo[a][0] == userName[0]:
+            alreadyExists = True
+    if not alreadyExists:
+        userInfo.append(userName)
+    userListLock.release()
+    return not alreadyExists
+# End of function add_user_to_list
+
+
+#####################################
+# Function: get_user_list
+#####################################
+def get_user_list():
+    userListLock.acquire()
+    names = []
+    for a in range(0, len(userInfo)):
+        names.append(userInfo[a][0])
+    userListLock.release()
+    return names
+# End of function get_user_list
+
+
+#####################################
+# Function: remove_user_from_list
+#####################################
+def remove_user_from_list(userName):
+    userListLock.acquire()
+    inList = False
+    index = -1
+    for a in range(0, len(userInfo)):
+        if userInfo[a][0] == userName:
+            inList = True
+            index = a
+    if inList:
+        userInfo.pop(index)
+    userListLock.release()
+    return inList
+# End of function remove_user_from_list
+
+
+#####################################
+# Function: get_user_socket
+#####################################
+def get_user_socket(userName):
+    userListLock.acquire()
+    output = ()
+    for a in range(0,len(userInfo)):
+        if userInfo[a][0] == userName:
+            output = userInfo[a][1]
+    userListLock.release()
+    return output
+# End of function get_user_socket
+
+
+#####################################
+# Function: handler
+#####################################
+def handler(connectionSocket, address):
+    message = connectionSocket.recv(4096)
+    message = message.decode()
+    request = message.split("\t")[0]
+
+    # Parse the method name from the request message
+    methodName = request.split('\t')[0].strip()
+
+    if(methodName.upper() == "LOGIN"):
+        loginProcessing(message, connectionSocket, address)
+    elif(methodName.upper() == "MESSAGE"):
+        messageProcessing(message,connectionSocket)
+    elif(methodName.upper() == "LOGOUT"):
+        logoutProcessing(message,connectionSocket)
+    elif(methodName.upper() == "USERLIST"):
+        getListProcessing(connectionSocket)
+    else:
+        print("Something went wrong")
 
     connectionSocket.close()
 # End of function handler
@@ -70,17 +165,21 @@ def handler(connectionSocket):
 
 # Client Listening Socket
 serverPort = 12012
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPort))
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
 serverSocket.listen(10)
-print ('The server is ready!')
+
+userInfo = []
+userListLock = threading.Lock()
+
+#executor = ThreadPoolExecutor(max_workers=5)
+print('The server is ready!')
 print("Current time is", datetime.now());
 
 
 # Main Program Loop
-while 1:
+while True:
     connectionSocket, addr = serverSocket.accept()
-    print("Connected")
-    _thread.start_new_thread(handler, (connectionSocket,))
-
-
+    #fut = executor.submit(handler, (connectionSocket, addr))
+    handThread = threading.Thread(target=handler, args=(connectionSocket, addr[0]))
+    handThread.start()
